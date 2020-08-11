7d8f13e5926d50884a1d1a1e9cb81efe61f5b019
askirmas <kirmas@phystech.edu>
2020-08-11 12:20:09 +0300

    Add record actions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,36 @@
-import {PureComponent} from 'react'
+import {PureComponent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
 import Card from '../components/Card'
 
 type tProps = {data: Record<string, tRecord>}
+type tFormButton = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
+  "type": string
+} & Partial<{
+  "value": string
+  "form": string 
+}>
 
 const {keys: $keys} = Object
+, submitsData: tFormButton[] = [
+  {
+    "type": "submit",
+    "value": "delete",
+
+  },
+  {
+    "type": "submit",
+    "value": "submit"
+  },
+  {
+    "type": "checkbox",
+    "form": ""
+  }
+]
+, submits = <>{submitsData.map(({value, type}) => <input {...{
+  type,
+  "className": `Button Card__${value}`,
+  "key": value,
+  value
+}}/>)}</>
 
 export default class Page extends PureComponent<tProps> {
   //@ts-ignore https://github.com/zeit/next.js/blob/d190f2e112bd4a767a21d1b0e9d61379d72ef1dc/packages/next/types/index.d.ts#L49
@@ -15,13 +42,14 @@ export default class Page extends PureComponent<tProps> {
 
   render() {
     const {data} = this.props
-    
+
     return <>{
       $keys(data)
       .map(id => <form {...{
-        "key": id
+        "key": id,
+        "className": "Card"
       }}>
-        <Card {...data[id]}/>
+        <Card {...data[id]}>{submits}</Card>
       </form>)
     }</>
   }  
